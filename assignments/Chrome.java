8aa5cf045e7f359dc01fd5acc739bdfd644428c2
eswariNagarathinam <135771419+eswariNagarathinam@users.noreply.github.com>
2024-12-16 07:04:28 +0530

    Add files via upload
    Week1 day1 Assignments

---
+++ assignments/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1.assignments;
+//Create a Java class named Chrome with variables of various data types and values.
+public class Chrome {
+	
+	public static void main(String args[]) {
+		float version=91.0f;
+		String developer="Google";
+		Boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		System.out.println("version  value "+ version);
+		System.out.println("developer "+ developer);
+		System.out.println("isBeta "+ isBeta);
+		System.out.println("releaseYear "+ releaseYear);
+		System.out.println("shortcutKey "+ shortcutKey);	
+				
+	}
+	
+
+}
